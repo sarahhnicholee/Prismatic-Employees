1e65ef7869c1036b74c7284bbd614c59abf343b5
Sarah Parent <sarahparent@Sarahs-MacBook-Air.local>
2025-01-22 19:37:22 -0600

    ouf

---
+++ server.js
@@ -0,0 +1,30 @@
+const { Prisma } = require("@prisma/client");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// Logging middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.originalUrl}`);
+  next();
+});
+
+app.use("/employees", require("./api/employees"));
+
+// 404
+app.use((req, res, next) => {
+  next({ status: 404, message: "Endpoint not found." });
+});
+
+// Error-handling
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.status ?? 500);
+  res.json(err.message ?? "Sorry, something went wrong :(");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
